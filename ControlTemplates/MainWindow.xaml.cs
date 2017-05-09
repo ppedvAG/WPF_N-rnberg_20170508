4dc3577ad62e9c2947acaeac44c119c026757686
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-05-09 14:22:17 +0200

    Control Templates

---
+++ ControlTemplates/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace ControlTemplates
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hallo!", "Die Nachricht:", MessageBoxButton.OK, MessageBoxImage.Hand);
+        }
+
+        private void NewWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            new MainWindow().Show();
+        }
+    }
+}
