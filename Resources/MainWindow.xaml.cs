1332cc49d80ce35d968f344a919357db97aa7387
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-05-09 11:19:14 +0200

    Resources

---
+++ Resources/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Resources
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeColorButton_Click(object sender, RoutedEventArgs e)
+        {
+            var brush = FindResource("defaultBackgroundColor") as SolidColorBrush;
+            //var brush = Resources["defaultBackgroundColor"] as SolidColorBrush;
+
+            if (brush != null)
+                brush.Color = Colors.Honeydew;
+        }
+
+        private void ChangeInstanceButton_Click(object sender, RoutedEventArgs e)
+        {
+            Resources["defaultBackgroundColor"] = new SolidColorBrush(Colors.MediumPurple);
+        }
+
+        private void ChangeTypeButton_Click(object sender, RoutedEventArgs e)
+        {
+            var brush = new RadialGradientBrush();
+            brush.GradientStops.Add(new GradientStop(Colors.White, 0));
+            brush.GradientStops.Add(new GradientStop(Colors.Black, 1));
+
+            Resources["defaultBackgroundColor"] = brush;
+        }
+    }
+}
