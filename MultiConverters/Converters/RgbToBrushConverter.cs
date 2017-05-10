f2c442eca36211a51d101243b1f3b5daa32f92fb
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-05-10 13:42:22 +0200

    MultiConverter

---
+++ MultiConverters/Converters/RgbToBrushConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace MultiConverters.Converters
+{
+    public class RgbToBrushConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var alpha = (byte)(double)values[0];
+            var red = (byte)(double)values[1];
+            var green = (byte)(double)values[2];
+            var blue = (byte)(double)values[3];
+
+            return new SolidColorBrush(Color.FromArgb(alpha, red, green, blue));
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            var color = (value as SolidColorBrush).Color;
+
+            return new object[]
+            {
+                (double)color.A,
+                (double)color.R,
+                (double)color.G,
+                (double)color.B,
+            };
+        }
+    }
+}
