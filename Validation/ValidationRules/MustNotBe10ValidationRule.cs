76fda32ba3e1d4b96cc11789ce12313cbc86a863
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-05-10 17:30:03 +0200

    Validation

---
+++ Validation/ValidationRules/MustNotBe10ValidationRule.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace Validation.ValidationRules
+{
+    public class MustNotBe10ValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var text = (string)value;
+
+            if(double.TryParse(text, out double zahl))
+            {
+                if (zahl == 10)
+                    return new ValidationResult(false, "Value must not be 10.");
+
+                return ValidationResult.ValidResult;
+            }
+
+            return new ValidationResult(false, $"'{text}' could not be converted.");
+        }
+    }
+}
